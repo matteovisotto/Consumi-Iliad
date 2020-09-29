f3394a0376f8c1b8f9dcc578c84557d689f5cb80
Matteo Visotto <matteo.visotto@mail.polimi.it>
2020-09-29 21:31:01 +0200

    Initial Commit

---
+++ Consumi IliadTests/Consumi_IliadTests.swift
@@ -0,0 +1,33 @@
+//
+//  Consumi_IliadTests.swift
+//  Consumi IliadTests
+//
+//  Created by Matteo Visotto on 29/09/2020.
+//
+
+import XCTest
+@testable import Consumi_Iliad
+
+class Consumi_IliadTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
